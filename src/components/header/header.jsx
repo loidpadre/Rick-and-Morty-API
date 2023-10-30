3f2779ed93b93c3931a24c860f3a829a3dd7d68b
Loid Padre <loidpadre@gmail.com>
2023-10-29 22:04:05 -0300

    Alterando o icon da ai, para gi

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,11 @@
-import {AiFillSnippets, AiFillHome} from "react-icons/ai"
+import {AiFillHome} from "react-icons/ai"
+import {GiFlamedLeaf} from "react-icons/gi"
 import { Wrapper } from "./style"
-import { Link } from "react-router-dom"
 export default function Header(){
     return(
         <Wrapper>
             <div className="logo">
-                <a href="/"><AiFillSnippets color="#213547" size={70}/></a>
+                <a href="/"><GiFlamedLeaf color="#213547" size={70}/></a>
                 <h1>R&M</h1>
             </div>
             <div className="navBar">
